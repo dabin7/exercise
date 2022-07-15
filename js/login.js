b42a8a948ea9c6e22c2967743e9233b8c425d1b9
dabin7 <brian421@naver.com>
2022-07-15 20:48:13 +0900

    Feat: login-form, clock

---
+++ js/login.js
@@ -0,0 +1,29 @@
+const loginForm = document.querySelector(".login-form")
+const loginInput = loginForm.querySelector("input")
+const greeting = document.querySelector(".greeting")
+
+const HIDDEN_CLASSNAME = "hidden";
+const NICKNAME_KEY = "nickname";
+
+function loginNameSubmit(event) {
+    event.preventDefault();
+    const nickname = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(NICKNAME_KEY, nickname);
+    painGreetings(nickname)
+}
+
+function painGreetings(nickname){
+    greeting.innerText = `${nickname}, Goal`
+    greeting.classList.remove(HIDDEN_CLASSNAME); 
+
+}
+
+const saveUsername = localStorage.getItem(NICKNAME_KEY)
+
+if(saveUsername === null ) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit" , loginNameSubmit);
+} else {
+    painGreetings(saveUsername);
+}
