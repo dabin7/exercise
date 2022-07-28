d4cde4b08efa67f6d11c31f57159aa1d7ed4f802
dabin7 <brian421@naver.com>
2022-07-28 09:42:04 +0900

    re 7.27

--- b14681/app.js
+++ b14681/app.js
@@ -1,25 +1,24 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-let input = [];
-rl.on('line', function (line) {
-    input.push(line);
-}).on('close', function () {
-    input = input.map((item) => +item);
-    solution(input[0], input[1]);
-    process.exit();
-});
+const fs = require("fs");
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.text';
+let input = fs.readFileSync(filePath).toString().split('\n');
 
-function solution(x, y){
+input = input.map((item) => +item);
+
+solution(input[0],input[1]);
+
+function solution(x,y){
+    let ans = 0;
     if(x>0 && y>0){
-        console.log("1")
-    } if(x<0 && y>0){
-        console.log("2")
-    } if(x<0 && y<0){
-        console.log("3")
-    } if(x>0 && y<0){
-        console.log("4")
-    } 
-    };
+        ans = 1;
+    }
+    if(x<0 && y>0){
+        ans = 2;
+    }
+    if(x<0 && y<0){
+        ans = 3;
+    }
+    if(x>0 && y<0){
+        ans = 4;
+    }
+    console.log(ans)
+}
--- b2588/app.js
+++ b2588/app.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
-input = input.map((item) => +item);
-solution(input[0], input[1]);
 
-function solution(A , B) {
-    const stringB = String(B)
-    const one = +stringB[2]
-    const ten = +stringB[1]
-    const hun = +stringB[0]
-    console.log(A * one)//3 472*5
-    console.log(A * ten)//4 472*8
-    console.log(A * hun)//5 472*3
-    console.log(A * B);//6 472*385
+input = input.map((item) => +item);
+
+solution(input[0],input[1]);
+
+function solution(A, B){
+    const strB = String(B)
+    const th = A * +strB[2]
+    const fo = A * +strB[1]
+    const fi = A * +strB[0]
+    const si = A * B
+    console.log(th)
+    console.log(fo)
+    console.log(fi)
+    console.log(si)
 }
