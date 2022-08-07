ed71f6fe6ffd01b93924f022c45fd65eaae4a2f8
dabin7 <brian421@naver.com>
2022-08-07 23:58:47 +0900

    retry

--- b1157/app.js
+++ b1157/app.js
@@ -4,5 +4,24 @@ let input = fs.readFileSync(filePath).toString().split('\n');
 solution(input[0]);
 
 function solution(str){
-   
+   const  X = str.toUpperCase()
+   const arr = new Array(26).fill(0);
+
+   for (let i = 0; i < X.length; i++){
+       const num = (X.charCodeAt(i))-65
+       arr[num]++
+    }
+
+    const max = Math.max(...arr);
+    const index = arr.indexOf(max);
+
+    let isSame = false;
+
+    for(let j = 0; j < 26; j++){
+        if(arr[j] === max && index !== j){
+            isSame = true;
+            break;
+        }
+    }
+    console.log(isSame ? "?" : String.fromCharCode(index + 65))
 }
