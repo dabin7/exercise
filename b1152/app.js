e8c3bf04cb53e723a9c8190929e7a597383bb5ba
dabin7 <brian421@naver.com>
2022-08-19 12:48:32 +0900

    retry

--- b1152/app.js
+++ b1152/app.js
@@ -1,16 +1,14 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
 solution(input[0]);
 
 function solution(str) {
-    let splitedStr = str.split(' ')
-    
-    let cnt = 0;
-    for (let i = 0; i < splitedStr.length; ++i){
-        if(splitedStr[i].length !== 0){
-            cnt++
-        }
+  const item = str.split(" ");
+  let cnt = 0;
+  for (let i = 0; i < item.length; i++)
+    if (item[i] !== "") {
+      cnt++;
     }
-    console.log(cnt)
+  console.log(cnt);
 }
--- b2908/app.js
+++ b2908/app.js
@@ -1,29 +1,20 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
-let splitedInput = input[0].split(' ');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+let splitedInput = input[0].split(" ");
 const reversePrint = (str) => {
-    let output = '';
-    for (let i = 2; i >= 0; --i) {
-        output += str[i];
-    }
-    console.log(output);
+  let output = "";
+  for (let i = 2; i >= 0; --i) {
+    output += str[i];
+  }
+  console.log(output);
 };
 solution(splitedInput[0], splitedInput[1]);
 
-function solution(A, B){
-    const strA = String(A)
-    const strB = String(B)
-    let arrA = []
-    let arrB = []
-    for (let i = 2; i >= 0; --i){
-        arrA.push(strA[i])
-        arrB.push(strB[i])
-    }
-    if(+arrA.join('') < +arrB.join('')){
-        console.log(+arrB.join(''))
-    } else{
-        console.log(+arrA.join(''))
-    }
+function solution(A, B) {
+  const reverseA = A.split(" ").map((x) => [...x].reverse().join(""));
+  const reverseB = B.split(" ").map((x) => [...x].reverse().join(""));
+
+  console.log(Math.max(reverseA, reverseB));
 }
 // +[...A].reverse().join() 으로 더 쉽게 가능
