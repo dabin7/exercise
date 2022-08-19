494d0642073544b8209378618363b9d9254c6ffd
dabin7 <brian421@naver.com>
2022-08-19 23:11:10 +0900

    Feat:

--- mine.js
+++ mine.js
@@ -7,7 +7,7 @@ const CODE = {
   NORMAL: -1,
   QUESTION: -2,
   FLAG: -3,
-  QUESTION_MIME: -4,
+  QUESTION_MINE: -4,
   FLAG_MINE: -5,
   MINE: -6,
   OPENED: 0, //0이상 다 열린칸
@@ -29,6 +29,7 @@ function plantMine() {
     shuffle.push(chosen);
   }
   const data = [];
+
   for (let i = 0; i < row; i++) {
     const rowData = [];
     data.push(rowData);
@@ -38,12 +39,46 @@ function plantMine() {
   }
 
   for (let k = 0; k < shuffle.length; k++) {
-    const ver = Math.floor(shuffle[k] / cell); // 몇번째 줄인지
-    const hor = shuffle[k] % cell; // 몇번째 칸인지
+    const ver = Math.floor(shuffle[k] / cell); // 몇번째 줄인지 숫자앞자리
+    const hor = shuffle[k] % cell; // 몇번째 칸인지 숫자뒷자리
     data[ver][hor] = CODE.MINE;
   }
   return data;
 }
+
+function onRightClick(event) {
+  event.preventDefault();
+  const target = event.target;
+  const rowIndex = target.parentNode.rowIndex;
+  const cellIndex = target.cellIndex;
+  const cellData = data[rowIndex][cellIndex];
+  if (cellData === CODE.MINE) {
+    data[rowIndex][cellIndex] = CODE.QUESTION_MINE;
+    target.className = "question";
+    target.textContent = "?";
+  } else if (cellData === CODE.QUESTION_MINE) {
+    data[rowIndex][cellIndex] = CODE.FLAG_MINE;
+    target.className = "flag";
+    target.textContent = "!";
+  } else if (cellData === CODE.FLAG_MINE) {
+    data[rowIndex][cellIndex] = CODE.MINE;
+    target.className = "";
+    target.textContent = "X";
+  } else if (cellData === CODE.NORMAL) {
+    data[rowIndex][cellIndex] = CODE.QUESTION;
+    target.className = "question";
+    target.textContent = "?";
+  } else if (cellData === CODE.QUESTION) {
+    data[rowIndex][cellIndex] = CODE.FLAG;
+    target.className = "flag";
+    target.textContent = "!";
+  } else if (cellData === CODE.FLAG) {
+    data[rowIndex][cellIndex] = CODE.NORMAL;
+    target.className = "";
+    target.textContent = "";
+  }
+}
+
 function drawTable() {
   data = plantMine();
   data.forEach((row) => {
@@ -53,9 +88,10 @@ function drawTable() {
       if (cell === CODE.MINE) {
         td.textContent = "X";
       }
-      tr.append(td);
+      tr.append(td); // append 와 appendchild 의 차이 : return값 반환 and DOMstring을 받을수있냐 아니냐의 차이
     });
     tbody.append(tr);
+    tbody.addEventListener("contextmenu", onRightClick); //이벤트버블링
   });
 }
 drawTable();
